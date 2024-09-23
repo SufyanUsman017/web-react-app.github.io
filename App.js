3180f496cb6cb7c2ed4effbd7dfabad240b71592
SufyanUsman017 <162179435+SufyanUsman017@users.noreply.github.com>
2024-09-24 03:34:07 +0500

    My-React-App

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div >
+      <center>
+        <h1>My React Web App</h1>
+        <h1>My React Web App</h1>
+        <h1>My React Web App</h1>
+        <h1>My React Web App</h1>
+        <h1>My React Web App</h1>
+      </center>
+    </div>
+  )
+}
+
+export default App
